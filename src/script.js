92df362f0eb494086124e2a1bbd4fda81103e6d3
lexi2345 <lexicantrell7@gmail.com>
2025-09-24 03:46:57 -0400

    fixed broken code

--- src/script.js
+++ src/script.js
@@ -14,12 +14,12 @@ function generatePoem(event) {
   let apiKey = "96a7o4155t404cfae6b739ecfd1b06ea";
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a American poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a American poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
   
 
 
